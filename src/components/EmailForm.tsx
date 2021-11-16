11429669cf80142169de9979fcb64845404fa343
plopez062500 <plopez062500@gmail.com>
2021-11-16 16:02:35 -0600

    Finished EmailForm Component

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,7 +1,11 @@
+import React, { useState } from 'react';
 import {
+  Flex,
   FormControl,
   FormLabel,
-  Input
+  Input,
+  Textarea,
+  Button
 } from '@chakra-ui/react';
 
 export type EmailFormData = {
@@ -16,11 +20,50 @@ interface EmailFormProps {
 }
 
 function EmailForm(props: EmailFormProps) {
-  const { onSubmit } = props
+
+  const { onSubmit } = props;
+
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function submit() {
+    if (onSubmit)
+      onSubmit({
+        fname: fname,
+        lname: lname,
+        email: email,
+        message: message
+      });
+  }
 
   return (
     <>
+      <Flex gridGap={5}>
+        <FormControl id="fname">
+          <FormLabel>First Name</FormLabel>
+          <Input type="text" placeholder='First name' value={fname} onChange={(e) => { setFname(e.target.value) }} />
+        </FormControl>
 
+        <FormControl id="lname">
+          <FormLabel>Last Name</FormLabel>
+          <Input type="text" placeholder='Last name' value={lname} onChange={(e) => { setLname(e.target.value) }} />
+        </FormControl>
+      </Flex>
+
+
+      <FormControl id="email">
+        <FormLabel>Email Name</FormLabel>
+        <Input type="email" placeholder='Email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+      </FormControl>
+
+      <FormControl id="message">
+        <FormLabel>Message</FormLabel>
+        <Textarea type="text" placeholder='Message' value={message} onChange={(e) => { setMessage(e.target.value) }} />
+      </FormControl>
+
+      <Button bg='primary.100' color='white' fontWeight='regular' _hover={{ bg: 'primary.200' }} isFullWidth my={4} onClick={submit}>Submit</Button>
     </>
   )
 }
